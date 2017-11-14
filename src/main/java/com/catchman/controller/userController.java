ce5e32acfad6c072cab051181b48e07888a72238
yuanfan <1016127734@qq.com>
2017-11-14 19:51:13 +0800

    第一次提交

---
+++ src/main/java/com/catchman/controller/userController.java
@@ -0,0 +1,24 @@
+package com.catchman.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+
+    private Logger logger = LoggerFactory.getLogger(userController.class);
+    /**
+     * 测试hello
+     * @return
+     */
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("name","Dear");
+        return new String (model.toString());
+    }
+
+}
