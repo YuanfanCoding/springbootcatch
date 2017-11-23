490b2077bf0d446bea709dbcbda3500a970086e9
yuanfan <1016127734@qq.com>
2017-11-23 20:27:09 +0800

    [新增]html页面；引入thymeleaf包；页面跳转控制Controller

---
+++ src/main/java/com/catchman/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.catchman.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PageController {
+    private Logger logger = LoggerFactory.getLogger(PageController.class);
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String login(Model model) {
+        return "index";
+    }
+}
